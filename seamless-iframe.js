a32ac3f5302f039538a596ef0b8f5c34869b36f1
Stephen Hurwitz <shurwitz@jawbone.com>
2014-01-21 11:34:21 -0800

    Added checks for allowed_domain

--- seamless-iframe.js
+++ seamless-iframe.js
@@ -5,17 +5,38 @@
      *
      * This function sends the height of the document to the parent. This is so
      * those pages can set the iframe to an appropriate height.
-     *
-     * You may wish to change the value of allowed_domain to the one you are
-     * targeting. By default the targetOrigin is '*' meaning all domains.
      */
-    var allowed_domain = '*',
-        sendHeight = function() {
-            win.parent.postMessage({
-                height: doc.body.scrollHeight,
-                href: win.location.href
-            }, allowed_domain);
-        };
+
+
+    /**
+     * Add and remove domains to prevent untrusted windows from executing code.
+     */
+    var allowed_domains = ['http://localhost:9000', '127.0.0.1'],
+
+    /**
+     * By default, the targetOrigin of window.postMessage is * meaning all
+     * domains. In some cases you will want to restrict this to help prevent
+     * XSS and other unscrupulus uses.
+     */
+    target_domain = '*',
+
+
+    checkDomain = function(origin) {
+        for (i = 0; i < domain_count; i++) {
+            if (allowed_domains[i] === origin) {
+                return true;
+            }
+        }
+        return false;
+    },
+
+
+    sendHeight = function() {
+        win.parent.postMessage({
+            height: doc.body.scrollHeight,
+            href: win.location.href
+        }, target_domain);
+    };
 
 
     /**
@@ -23,6 +44,11 @@
      * report the document height.
      */
     win.onmessage = function(e) {
+
+        if (!checkDomain(e.origin)) {
+            return;
+        }
+
         if (e.data.hasOwnProperty('request') && e.data.request === 'height') {
             sendHeight();
             win.onmessage = null;
--- seamless-parent.js
+++ seamless-parent.js
@@ -1,53 +1,73 @@
 (function (win, doc) {
 
+    /**
+     * Add and remove domains to prevent untrusted windows from executing code.
+     */
+    var allowed_domains = ['http://localhost', 'http://127.0.0.1'],
 
     /**
      * By default, the targetOrigin of window.postMessage is * meaning all
      * domains. In some cases you will want to restrict this to help prevent
      * XSS and other unscrupulus uses.
      */
-    var allowed_domain = '*',
+    target_domain = '*',
 
-        extra_space = 60,
+    extra_space = 60,
 
-        iframes = doc.getElementsByTagName('iframe'),
-        iframes_count = iframes.length,
-        frame, i,
-
-
-        getTarget = function(frame) {
-            return (frame.contentWindow || frame.contentDocument);
-        },
+    iframes = doc.getElementsByTagName('iframe'),
+    iframes_count = iframes.length,
+    domain_count = allowed_domains.length,
+    frame, i,
 
 
-        setFrameHeight = function(e) {
-            if (e.data.hasOwnProperty('height') && e.data.hasOwnProperty('href')) {
-                for (i = 0; i < iframes_count; i++) {
-                    frame = iframes[i];
-                    if (getTarget(frame).location.href === e.data.href) {
-                        frame.style.height = (parseInt(e.data.height, 10) || 0 ) + extra_space + 'px';
-                        frame = null;
-                        return;
-                    }
-                }
-            }
-        };
-
-
-        for (i = 0; i < iframes_count; i++) {
-            frame = iframes[i];
-            if (frame.getAttribute('seamless') !== null) {
-                getTarget(frame).postMessage({
-                    request: 'height'
-                }, allowed_domain);
+    checkDomain = function(origin) {
+        for (i = 0; i < domain_count; i++) {
+            if (allowed_domains[i] === origin) {
+                return true;
             }
         }
-        frame = null;
+        return false;
+    },
 
 
-        window.addEventListener('message', function(e) {
-            setFrameHeight(e);
-        });
+    getTarget = function(frame) {
+        return (frame.contentWindow || frame.contentDocument);
+    },
+
+
+    setFrameHeight = function(e) {
+
+        if (!checkDomain(e.origin)) {
+            return;
+        }
+
+        if (e.data.hasOwnProperty('height') && e.data.hasOwnProperty('href')) {
+            for (i = 0; i < iframes_count; i++) {
+                frame = iframes[i];
+                if (getTarget(frame).location.href === e.data.href) {
+                    frame.style.height = (parseInt(e.data.height, 10) || 0 ) + extra_space + 'px';
+                    frame = null;
+                    return;
+                }
+            }
+        }
+    };
+
+
+    for (i = 0; i < iframes_count; i++) {
+        frame = iframes[i];
+        if (frame.getAttribute('seamless') !== null) {
+            getTarget(frame).postMessage({
+                request: 'height'
+            }, target_domain);
+        }
+    }
+    frame = null;
+
+
+    window.addEventListener('message', function(e) {
+        setFrameHeight(e);
+    });
 
 })(window, document);
 
